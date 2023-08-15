22238b6e2150a2d2ed5585366f2ee667f98aa5d4
Indrajit Ghosh <indrajitghosh.dev@gmail.com>
2023-08-15 15:50:11 +0530

    rtggre

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,39 @@
 jQuery(document).ready(function ($) {
-    "use strict";
-    $("#testimonials-list").owlCarousel({
-      loop: true,
-      center: true,
-      items: 3,
-      margin: 0,
-      autoplay:true,
-      dots: true,
-      autoplayTimeout: 2000,
-      smartSpeed: 450,
-      responsive: {
-        0: {
-          items: 1,
-        },
-        768: {
-          items: 2,
-        },
-        1280: {
-          items: 3,
-        },
+  "use strict";
+  var testimonialsSlider = $("#testimonials-list");
+  
+  testimonialsSlider.owlCarousel({
+    loop: true,
+    center: true,
+    items: 3,
+    margin: 0,
+    autoplay: false,
+    dots: true,
+    autoplayTimeout: 2000,
+    smartSpeed: 450,
+    responsive: {
+      0: {
+        items: 1,
       },
-    });
+      768: {
+        items: 2,
+      },
+      1280: {
+        items: 3,
+      },
+    },
   });
 
-// $('.owl-carousel').owlCarousel({
-//   margin:10,
-//   loop:true,
-//   autoWidth:true,
-//   items:4,
-//   responsive: {
-//             0: {
-//               items: 1,
-//             },
-//             768: {
-//               items: 2,
-//             },
-//             1280: {
-//               items: 3,
-//             },
-//           },
-// })
+  // Control Buttons
+  var prevButton = $(".prev-button");
+  var nextButton = $(".next-button");
 
-    
+  prevButton.click(function () {
+    testimonialsSlider.trigger("prev.owl.carousel");
+  });
+
+  nextButton.click(function () {
+    testimonialsSlider.trigger("next.owl.carousel");
+  });
+});
+
